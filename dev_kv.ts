b2f1848ada303ac959dae769f15f9a81ca9ca222
James Bradlee <james@nslo.no>
2025-09-10 07:30:08 +0200

    fix: kv path on different platforms

--- dev_kv.ts
+++ dev_kv.ts
@@ -1,5 +1,6 @@
+import { fromFileUrl } from "@std/path";
 import { setKv } from "./kv.ts";
 
-const path = import.meta.resolve("./kv.db").substring(7);
+const path = fromFileUrl(import.meta.resolve("./kv.db"));
 console.debug("debug: Opening KV at %s", path);
 setKv(await Deno.openKv(path));
