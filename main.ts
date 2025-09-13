fabcd03d981e002cf86686897c5122ba76c12d37
James Bradlee <james@nslo.no>
2025-09-13 10:20:38 +0200

    fix: add signal listeners to gracefully shutdown server and release resources

--- main.ts
+++ main.ts
@@ -1,9 +1,25 @@
 import { app } from "./app.ts";
+import { closeKv } from "./kv.ts";
+
+const ac = new AbortController();
+
+Deno.addSignalListener("SIGINT", () => {
+  console.debug("debug: caught SIGINT, shutting down...");
+  ac.abort();
+});
+
+Deno.addSignalListener("SIGTERM", () => {
+  console.debug("debug: caught SIGTERM, shutting down...");
+  ac.abort();
+});
+
+ac.signal.addEventListener("abort", closeKv);
 
 const server = Deno.serve({
   onListen: ({ hostname, port }) => {
     console.debug(`debug: listening on http://${hostname}:${port}`);
   },
+  signal: ac.signal,
 }, app);
 
 await server.finished;
