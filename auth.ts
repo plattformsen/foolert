c7143f380c78891b48a5cc00b84b728c4a04e7b0
James Bradlee <james@nslo.no>
2025-09-13 10:14:06 +0200

    refactor: improve baseHostname function documentation and logic

--- auth.ts
+++ auth.ts
@@ -19,13 +19,27 @@ function normalizeHostname(hostname: string): string {
   return hostname;
 }
 
-function baseHostname(hostname: string): string | undefined {
-  // strip the first subdomain
-  const indexOfDot = hostname.indexOf(".");
-  if (indexOfDot === -1) {
-    return undefined;
+/**
+ * Returns the hostname without the first subdomain.
+ *
+ * The way this method works is that it removes everything
+ * leading up to and including the first dot. If there are
+ * no dots left, it returns a single dot.
+ *
+ * @param hostname The hostname to strip the first subdomain from
+ * @returns The hostname without the first subdomain.
+ */
+function baseHostname(hostname: string): string {
+  const indexOfFirstDot = hostname.indexOf(".");
+
+  if (indexOfFirstDot === -1) {
+    // In this case, we have reached the top-level domain,
+    // empty string, or a TLD (like "localhost") that hasn't
+    // been normalized.
+    return ".";
   }
-  return hostname.slice(indexOfDot + 1);
+
+  return hostname.slice(indexOfFirstDot + 1);
 }
 
 export async function authenticateRequest(
